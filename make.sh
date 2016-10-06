6c447196cecfaf45a19423f70128305a05ca1d27
Reto Stauffer <reto.stauffer@uibk.ac.at>
2016-10-06 20:16:08 +0200

    added meteodata to make.sh

--- make.sh
+++ make.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
+set -u
+
 # - Checking version
 version=`cat getgrib/DESCRIPTION | grep 'Version:' | awk '{print $2}'`
+host=`hostname`
 
-export PKG_FCFLAGS="-static-libgfortran -L/usr -I/usr/include -lgrib_api_f90 -lgrib_api"
-export PKG_LIBS="-L/usr -I/usr/include -lgrib_api_f90 -lgrib_api"
+if [ $host == "meteo-data.uibk.ac.at" ] ; then
+   export PKG_FCFLAGS="-L/opt/local_libs/lib -I/opt/local_libs/include -lgrib_api_f90 -lgrib_api"
+   export PKG_LIBS="-L/opt/local_libs/lib -I/opt/local_libs/include -lgrib_api_f90 -lgrib_api"
+else
+   exit 3
+   export PKG_FCFLAGS="-static-libgfortran -L/usr -I/usr/include -lgrib_api_f90 -lgrib_api"
+   export PKG_LIBS="-L/usr -I/usr/include -lgrib_api_f90 -lgrib_api"
+fi
 
 printf " BUILDING PACKAGE: getgrib\n"
 R CMD build --no-build-vignettes getgrib
